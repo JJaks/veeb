18d362240dc3477923505212e3892eaeb08540a2
Jaagop <jaagop.janson@khk.ee>
2020-02-19 08:44:07 +0000

    URL teisendamine ja kuvamine

--- app/libs/Core.php
+++ app/libs/Core.php
@@ -1,6 +1,25 @@
 <?php
 
 
-class Core
-{
+class Core {
+    protected $currentController = "Pages";
+    protected $currentMethod = "index";
+    protected $params = array();
+
+
+    public function __construct() {
+        $url = $this->getUrl();
+        echo '<pre>';
+        print_r($url);
+        echo '</pre>';
+    }
+
+    public function getUrl() {
+        if (isset($_GET['url'])) {
+            $url = rtrim(($_GET['url']), '/');
+            $url = filter_var($url, FILTER_SANITIZE_URL);
+            $url = explode("/", $url);
+            return $url;
+        }
+    }
 }
