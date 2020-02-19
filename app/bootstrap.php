eaf0489b95853c9cb90f02804c4ba74041704e08
Jaagop <jaagop.janson@khk.ee>
2020-02-19 07:47:09 +0000

    Süsteemialuse elemendid

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -1,2 +1,3 @@
 <?php
 require_once 'config/config.php';
+require_once 'libs/Core.php';
--- public/index.php
+++ public/index.php
@@ -1,2 +1,7 @@
 <?php
 require_once '../app/bootstrap.php';
+$init = new Core();
+
+echo '<pre>';
+print_r ($init);
+echo '</pre>';
