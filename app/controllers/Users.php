6b0e49c39a89d9e2fa14c42f24f403352c2b9df1
Jaagop <jaagopj@gmail.com>
2020-03-02 07:53:43 +0000

    Added registration thingamaduuhikies

--- app/controllers/Users.php
+++ app/controllers/Users.php
@@ -3,12 +3,18 @@
 
 class Users extends Controller
 {
-    public function login() {
+    public function __construct(){
+        $this->userModel = $this->model('User');
+    }
+
+    public function login(){
         $this->view('users/login');
     }
-    public function register() {
-        if($_SERVER ['REQUEST_METHOD'] == 'POST'){
-            // proccess form
+
+    public function register(){
+        // check post request
+        if($_SERVER['REQUEST_METHOD'] == 'POST'){
+            // process form
             $_POST = filter_input_array(INPUT_POST, FILTER_SANITIZE_STRING);
             // init data
             $data = array(
@@ -21,26 +27,42 @@ class Users extends Controller
                 'pass_err' => '',
                 'pass2_err' => ''
             );
-            if(empty($data['name'])) {
-                $data['name_err'] = 'Please enter name';
+            // validate name
+            if(empty($data['name'])){
+                $data['name_err'] = 'Please enter the name';
             }
-            if(empty($data['email'])) {
-                $data['email_err'] = 'Please enter email';
+            // validate email
+            if(empty($data['email'])){
+                $data['email_err'] = 'Please enter the email';
+            } else if($this->userModel->findUserByEmail($data['email'])){
+                $data['email_err'] = 'Email is already taken';
             }
-            if(empty($data['pass'])) {
-                $data['pass_err'] = 'Please enter password';
+            // validate password
+            if(empty($data['pass'])){
+                $data['pass_err'] = 'Please enter the password';
+            } else if(strlen($data['pass']) < 6){
+                $data['pass_err'] = 'Password must be at least 6 characters';
             }
-            if(empty($data['pass2'])) {
-                $data['pass2_err'] = 'Please confirm password';
+            // validate password confirmation
+            if(empty($data['pass2'])){
+                $data['pass2_err'] = 'Please enter the confirm password';
+            } else if($data['pass'] != $data['pass2']){
+                $data['pass2_err'] = 'Passwords do not match';
             }
-            echo '<pre>';
-            print_r($data);
-            echo '</pre>';
-            $this->view('users/register',  $data);
-        }
-        else {
+            // if errors are empty - register user
+            if(empty($data['name_err']) and empty($data['email_err']) and empty($data['pass_err']) and empty($data['pass2_err'])){
+                //hash password
+                $data['pass'] = password_hash($data['pass'], PASSWORD_DEFAULT);
+                if($this->userModel->register($data)){
+                    header('Location: '.URLROOT.'/users/login');
+                } else {
+                    die('Sometrhing went wrong');
+                }
+            } else {
+                $this->view('users/register', $data);
+            }
+        } else {
             $this->view('users/register');
         }
-
     }
 }
